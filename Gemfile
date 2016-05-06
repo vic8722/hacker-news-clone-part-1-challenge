70130467f60f337c699d2d6f93db2fa64320154b
b0uma <b0uma@users.noreply.github.com>
2016-05-06 11:41:41 -0700

    Revert "Pair agurusa"

--- Gemfile
+++ Gemfile
@@ -25,6 +25,3 @@ group :test, :development do
   gem 'factory_girl'
   gem 'faker'
 end
-
-gem 'bcrypt'
-
--- app/models/tweets_user.rb
+++
@@ -1,5 +0,0 @@
-class TweetsUser < ActiveRecord::Base
-  # Remember to create a migration!
-  belongs_to :tweet
-  belongs_to :user
-end
