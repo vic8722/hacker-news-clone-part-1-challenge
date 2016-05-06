60b2fad37f936cf24272fe18880db26b2e8bf6b5
Andrew Crowley & Bill <pair+andyvegas.billdevcode@devbootcamp.com>
2016-05-05 18:27:48 -0700

    Add login page, create new user page and add bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,46 @@
-get '/users/login' do
+get '/' do
+  redirect '/login'
+end
+
+get '/login' do
   erb :'users/index'
 end
 
-get '/users/new' do
+post '/login' do
+   @user = User.find_by_email(params[:email])
+   if @user.authenticate(params[:password])
+     session[:id] = @user.id
+     session[:username] = @user.username
+     redirect '/users/show'
+   else
+     redirect '/'
+   end
+ end
+
+ post '/logout' do
+   session[:id] = nil
+   redirect '/'
+ end
+
+post '/create_user' do
+   unless User.find_by_email(params[:user['email']])
+     @user = User.new(params[:user])
+     @user.password = params[:password]
+     @user.save
+   end
+   redirect '/login'
+ end
+
+get '/register' do
   erb :'users/new'
 end
 
 get '/user/profile' do
+   if session[:id]
+     erb :'users/show'
+   else
+     redirect '/login'
+   end
   erb :'users/show'
 end
 
