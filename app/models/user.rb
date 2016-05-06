acb7ba5acdd2fca0b41dc280acb0cf16a09f4ec1
Aarthi <agurusa@gmail.com>
2016-05-05 19:22:02 -0700

    Finish database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
+  include BCrypt
   # Remember to create a migration!
   has_many :tweets, through: :tweet_users
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate(password)
+    self.password == password
+  end
 end
